4b9d948608d3ed14c8b83f1e3699347650f9f3b6
young <brevinyoung@gmail.com>
2021-10-27 21:57:11 -0700

    working on updating the routes

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,6 +3,14 @@ import React from "react";
 function Resume() {
   return (
     <div style={{ margin: "0 2em 0 2em" }}>
+      <div style={{ paddingBottom: "2em" }}>
+        <a
+          target="_blank"
+          href="https://docs.google.com/document/d/1Wl_Vz83OOuMWHPAfsHdrRQP-ldhvwpwDWUdZoU0lZKI/edit#heading=h.n64fgzu3lwuy"
+        >
+          Resume
+        </a>
+      </div>
       <h3>Front end experience</h3>
       <ul class="list-group list-group-flush" style={{ marginBottom: "2em" }}>
         <li class="list-group-item">HTML</li>
@@ -20,5 +28,6 @@ function Resume() {
       </ul>
     </div>
   );
+  // https://docs.google.com/document/d/1Wl_Vz83OOuMWHPAfsHdrRQP-ldhvwpwDWUdZoU0lZKI/edit#heading=h.n64fgzu3lwuy
 }
 export default Resume;
