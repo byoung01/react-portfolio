4e65d24d9eda2e5d2e8c8b0f9fb24d6f073d171c
young <brevinyoung@gmail.com>
2021-10-27 19:28:09 -0700

    functionality works

--- react-portfolio/src/components/Footer/index.js
+++ react-portfolio/src/components/Footer/index.js
@@ -2,29 +2,31 @@ import React from "react";
 
 function Footer() {
   return (
-    <nav class="nav" style={{ marginTop: "3em" }}>
-      <a
-        class="nav-link active"
-        href="https://github.com/byoung01"
-        target="_blank"
-      >
-        Github
-      </a>
-      <a
-        class="nav-link active"
-        href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_nav-header-signin"
-        target="_blank"
-      >
-        LinkedIn
-      </a>
-      <a
-        class="nav-link active"
-        href="https://github.com/byoung01"
-        target="_blank"
-      >
-        Git
-      </a>
-    </nav>
+    <div style={{ marginLeft: "3em" }}>
+      <nav class="nav" style={{ marginTop: "3em" }}>
+        <a
+          class="nav-link active"
+          href="https://github.com/byoung01"
+          target="_blank"
+        >
+          Github
+        </a>
+        <a
+          class="nav-link active"
+          href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_nav-header-signin"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+        <a
+          class="nav-link active"
+          href="https://github.com/byoung01"
+          target="_blank"
+        >
+          Git
+        </a>
+      </nav>
+    </div>
   );
 }
 
